0e48b07a0d82b965ced8bb23ee0878c35fb687cc
Sainathgit config --global user.name Sainathgit config --global user.name Sainath <sainath.valavala.31@gmail.com>
2025-12-11 12:20:37 +0530

    removed all code

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,12 @@
 import React from "react";
-import Counter from "./Counter";
 
 function App() {
-  function abc(val) {
-    //abc should receive data from parent so created a param named val.
-    alert(`called ${val ? val : " but data not sent from child"}`);
-  }
   return (
-    <div className="border border-5 p-3 m-3 border-primary">
+    <div>
       App
       <div>
-        <button
-          onClick={() => {
-            abc();
-          }}
-        >
-          Parent
-        </button>
+        <Counter></Counter>
       </div>
-      <div>
-        <Counter step={10} a={abc}>
-          100
-        </Counter>
-      </div>
-      <div>
-        <Counter step={5} a={abc}>
-          200
-        </Counter>
-      </div>
-      {/* <div>{Counter()}</div> */}
     </div>
   );
 }
--- src/Counter.jsx
+++ src/Counter.jsx
@@ -1,31 +1,9 @@
-import React, { useState } from "react";
+import React from 'react'
 
-function Counter(props) {
-  console.log(props);
-  let [count, setCount] = useState(+(props.children));
+function Counter() {
   return (
-    <div className="border border-3 border-info m-2 p-2">
-      <div>Counter :{count}</div>
-      <button
-        className="btn btn-success me-2"
-        onClick={() => {
-          setCount(count + props.step);
-        }}
-      >
-        Increment
-      </button>
-      <button
-        className="btn btn-danger"
-        onClick={() => {
-          setCount(count - 1);
-        }}
-      >
-        Decrement
-      </button>
-{/* send data from child to parent as an argument and the parent receives it */}
-      <button onClick={()=>{props.a("parent")}}>child</button>
-    </div>
-  );
+    <div>Counter</div>
+  )
 }
 
-export default Counter;
+export default Counter
